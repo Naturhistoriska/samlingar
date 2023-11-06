487dcd595f560a333058adfd2c5509ad4f48e843
Ida Li <ida.li@nrm.se>
2023-11-06 09:48:10 +0100

    Fix query

--- samgler-specify-dwc/specify-data-process/src/main/java/se/nrm/specify/data/process/logic/DataProcessor.java
+++ samgler-specify-dwc/specify-data-process/src/main/java/se/nrm/specify/data/process/logic/DataProcessor.java
@@ -129,36 +129,41 @@ public class DataProcessor implements Serializable {
     }
 
     private String buildJpqlSb() {
-        sb = new StringBuilder();
-         sb.append("SELECT DISTINCT c FROM Collectionobject c ")
-                .append("LEFT JOIN FETCH c.accession ")
+        sb = new StringBuilder(); 
+        sb.append("SELECT DISTINCT c FROM Collectionobject c ")
                 .append("LEFT JOIN FETCH c.collectionObjectAttribute ")
-                .append("LEFT JOIN FETCH c.determinationList d ")
+                .append("LEFT JOIN FETCH c.determinations d ")
                 .append("LEFT JOIN FETCH c.collection ct ")
+                .append("LEFT JOIN FETCH ct.discipline dsc ")
+                .append("LEFT JOIN FETCH dsc.division div ")
+                .append("LEFT JOIN FETCH div.institution inst ")
                 .append("LEFT JOIN FETCH c.collectingEvent ce ")
-                .append("LEFT JOIN FETCH c.preparationList p ")
-                .append("LEFT JOIN FETCH c.collectionobjectattachmentList ca ")
+                .append("LEFT JOIN FETCH c.preparations p ")
                 .append("LEFT JOIN FETCH ce.locality lc ")
-                .append("LEFT JOIN FETCH ce.collectorList clts ")
+                .append("LEFT JOIN FETCH ce.collectors clts ")
                 .append("LEFT JOIN FETCH clts.agent ")
+                .append("LEFT JOIN FETCH ce.collectingEventAttribute cea ")
                 .append("LEFT JOIN FETCH lc.geography g ")
+                .append("LEFT JOIN FETCH lc.localitydetails ld ")
+                .append("LEFT JOIN FETCH lc.geocoorddetails gd ")
+                .append("LEFT JOIN FETCH gd.agent gdagent ") 
                 .append("LEFT JOIN FETCH d.preferredTaxon pt ")
-                .append("LEFT JOIN FETCH pt.commonnametxList cn ")
-                .append("LEFT JOIN FETCH pt.synomys ")
-                .append("LEFT JOIN FETCH d.taxon t ")
-                .append("LEFT JOIN FETCH t.synomys ")
-                .append("LEFT JOIN FETCH t.commonnametxList cn ")
+                .append("LEFT JOIN FETCH pt.commonnametxs cn ")
+                .append("LEFT JOIN FETCH pt.taxonTreeDefItem tdi ") 
                 .append("LEFT JOIN FETCH d.determiner ")
-                .append("LEFT JOIN FETCH p.storage ")
                 .append("LEFT JOIN FETCH p.prepType ")
-                .append("LEFT JOIN FETCH ca.attachment a ")
-                .append("LEFT JOIN FETCH a.attachmentImageAttribute ai ")
-                .append("LEFT JOIN FETCH ai.morphBankWiew ")
-                .append("where c.collectionMemberID = :collectionMemberID ")
-                .append("AND c.collectionObjectID in :ids ")
-                .append("ORDER BY c.collectionObjectID");
-        return sb.toString().trim();
-//        sb.append("SELECT DISTINCT c FROM Collectionobject c ")
+                .append("LEFT JOIN FETCH p.storage s ")
+                .append("WHERE c.collectionMemberID = :collectionMemberID ");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         sb.append("SELECT DISTINCT c FROM Collectionobject c ")
 //                .append("LEFT JOIN FETCH c.accession ")
 //                .append("LEFT JOIN FETCH c.collectionObjectAttribute ")
 //                .append("LEFT JOIN FETCH c.determinationList d ")
@@ -185,70 +190,16 @@ public class DataProcessor implements Serializable {
 //                .append("where c.collectionMemberID = :collectionMemberID ")
 //                .append("AND c.collectionObjectID in :ids ")
 //                .append("ORDER BY c.collectionObjectID");
-//        return sb.toString().trim();
+        return sb.toString().trim();
 
-//        sb.append("SELECT DISTINCT c FROM Collectionobject c ")
-//                .append("LEFT JOIN FETCH c.accession ")
-//                .append("LEFT JOIN FETCH c.collectionObjectAttribute ")
-//                .append("LEFT JOIN FETCH c.determinationList d ")
-//                .append("LEFT JOIN FETCH c.collection ct ")
-//                .append("LEFT JOIN FETCH c.collectingEvent ce ")
-//                .append("LEFT JOIN FETCH c.preparationList p ")
-//                .append("LEFT JOIN FETCH c.collectionobjectattachmentList ca ")
-//                .append("LEFT JOIN FETCH ce.locality lc ")
-//                .append("LEFT JOIN FETCH ce.collectorList clts ")
-//                .append("LEFT JOIN FETCH clts.agent ")
-//                .append("LEFT JOIN FETCH lc.geography g ")
-//                .append("LEFT JOIN FETCH g.parent gp ")
-//                .append("LEFT JOIN FETCH gp.parent gpp ")
-//                .append("LEFT JOIN FETCH gpp.parent gppp ")
-//                .append("LEFT JOIN FETCH gppp.parent gpppp ")
-//                .append("LEFT JOIN FETCH gpppp.parent gppppp ")
-//                .append("LEFT JOIN FETCH gppppp.parent gpppppp ")
-//                .append("LEFT JOIN FETCH d.preferredTaxon pt ")
-//                .append("LEFT JOIN FETCH pt.commonnametxList cn ")
-//                .append("LEFT JOIN FETCH pt.synomys ")
-//                .append("LEFT JOIN FETCH pt.parent ptp ")
-//                .append("LEFT JOIN FETCH ptp.parent ptpp ")
-//                .append("LEFT JOIN FETCH ptpp.parent ptppp ")
-//                .append("LEFT JOIN FETCH ptppp.parent ptpppp ")
-//                .append("LEFT JOIN FETCH ptpppp.parent ptppppp ")
-//                .append("LEFT JOIN FETCH ptppppp.parent ptpppppp ")
-//                .append("LEFT JOIN FETCH ptpppppp.parent ptppppppp ")
-//                .append("LEFT JOIN FETCH ptppppppp.parent ptpppppppp ")
-//                .append("LEFT JOIN FETCH ptpppppppp.parent ptppppppppp ")
-//                .append("LEFT JOIN FETCH ptppppppppp.parent ptpppppppppp ")
-//                .append("LEFT JOIN FETCH d.taxon t ")
-//                .append("LEFT JOIN FETCH t.synomys ")
-//                .append("LEFT JOIN FETCH t.commonnametxList cn ")
-//                //    if (collectionId != ornithologyCollectionId) {
-//                //      sb.append("LEFT JOIN FETCH t.commonnametxs cn ");
-//                //    }
-//                .append("LEFT JOIN FETCH t.parent tp ")
-//                .append("LEFT JOIN FETCH tp.parent tpp ")
-//                .append("LEFT JOIN FETCH tpp.parent tppp ")
-//                .append("LEFT JOIN FETCH tppp.parent tpppp ")
-//                .append("LEFT JOIN FETCH tpppp.parent tppppp ")
-//                .append("LEFT JOIN FETCH tppppp.parent tpppppp ")
-//                .append("LEFT JOIN FETCH tpppppp.parent tppppppp ")
-//                .append("LEFT JOIN FETCH tppppppp.parent tpppppppp ")
-//                .append("LEFT JOIN FETCH tpppppppp.parent tppppppppp ")
-//                .append("LEFT JOIN FETCH tppppppppp.parent tpppppppppp ")
-//                .append("LEFT JOIN FETCH d.determiner ") 
-//                .append("LEFT JOIN FETCH p.prepType ")
-//                .append("LEFT JOIN FETCH ca.attachment a ")
-//                .append("LEFT JOIN FETCH a.attachmentImageAttribute ai ")
-//                .append("LEFT JOIN FETCH ai.morphBankWiew ") 
-//                .append("where c.collectionMemberID = :collectionMemberID ")
-//                .append("AND c.collectionObjectID in :ids ")
-//                .append("ORDER BY c.collectionObjectID");
     }
 
     private String buildGetUpdateIdsQuery() {
         log.info("buildGetUpdateIdsQuery");
         sb = new StringBuilder();
-        sb.append("SELECT c.collectionObjectID FROM Collectionobject c ");
-        sb.append("WHERE c.collectionMemberID = :collectionMemberID ");
+//        sb.append("SELECT c.collectionObjectID FROM Collectionobject c "); 
+//        sb.append("WHERE c.collectionMemberID = :collectionMemberID ");
+        sb.append(buildJpqlSb()); 
         sb.append("AND c.timestampModified BETWEEN :fromDate AND :toDate ");
         sb.append("ORDER BY c.collectionObjectID");
         return sb.toString();
