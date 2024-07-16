2e3dde81e52daaeb8cbbcaa55578c28300df73b5
Ida Li <ida.li@nrm.se>
2024-07-16 10:27:10 +0200

    Create SpeciesGroup.vue

---
+++ samlingar-ui/src/components/SpeciesGroup.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-select
+    :items="supportedCollections"
+    :label="$t('search.selectSpeciesGroup')"
+    :clearable="true"
+    density="compact"
+    max-width="500"
+    @update:modelValue="setSelected"
+  />
+</template>
+<script>
+export default {
+  name: 'SpeciesGroup',
+  data() {
+    return {
+      // selected: ''
+    }
+  },
+  computed: {
+    supportedCollections() {
+      return import.meta.env.VITE_SUPPORTED_COLLECTIONS.split(',')
+    }
+  },
+  methods: {
+    setSelected(value) {
+      console.log('selected...' + value)
+      this.$emit('species-group', value)
+    }
+  }
+}
+</script>
