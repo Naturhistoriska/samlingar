c72e3cad18ed3662845f4f1aa89fa2e4178351b3
Ida Li <ida.li@nrm.se>
2024-07-16 10:28:03 +0200

    Update index.js

--- samlingar-ui/src/store/index.js
+++ samlingar-ui/src/store/index.js
@@ -2,17 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    showDetail: false,
     showResults: false,
+    selectedResult: null,
     results: [],
     totalRecords: 0
   },
 
   getters: {
+    showDetail(state) {
+      return state.showDetail
+    },
     showResults(state) {
       return state.showResults
     },
     results(state) {
       return state.results
+    },
+    selectedResult(state) {
+      return state.selectedResult
     },
     totalRecords(state) {
       return state.totalRecords
@@ -20,8 +28,10 @@ export default createStore({
   },
 
   mutations: {
+    setShowDetail: (state, payload) => (state.showDetail = payload),
     setShowResults: (state, payload) => (state.showResults = payload),
     setResults: (state, payload) => (state.results = payload),
+    setSelectedResult: (state, payload) => (state.selectedResult = payload),
     setTotalRecords: (state, payload) => (state.totalRecords = payload)
   }
 
