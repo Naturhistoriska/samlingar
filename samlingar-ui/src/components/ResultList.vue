8f17158dd7b0319a8e9627f750a28def6f227c34
Ida Li <ida.li@nrm.se>
2024-07-16 10:26:53 +0200

    Create ResultList.vue

---
+++ samlingar-ui/src/components/ResultList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <legend>{{ $t('results.searchResults') }}</legend>
+    <div id="resultList">
+      <p>{{ $t('results.num_results', totalRecords) }}</p>
+      <template v-for="(result, index) in results" :key="result.uuid">
+        <Result v-bind:result="result" />
+        <v-divider
+          v-if="index + 1 < results.length"
+          :key="`devider-${index}`"
+          class="border-opacity-50"
+          color="white"
+        ></v-divider>
+      </template>
+    </div>
+  </div>
+</template>
+<script>
+import Result from '@/components/Result.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Results',
+  components: {
+    Result
+  },
+  data: () => ({}),
+
+  computed: {
+    ...mapGetters(['results', 'totalRecords'])
+  },
+
+  methods: {}
+}
+</script>
+<style scoped></style>
