364906a2bae5e63d6f172f30bbf938db15ee0885
Ida Li <ida.li@nrm.se>
2024-07-22 09:44:45 +0200

    Add store value

--- samlingar-ui/src/store/index.js
+++ samlingar-ui/src/store/index.js
@@ -2,25 +2,45 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    catalogNumber: null,
+    isAdvanceSearch: false,
+    results: [],
+    scientificName: null,
+    searchText: null,
+    selectedResult: null,
     showDetail: false,
     showResults: false,
-    selectedResult: null,
-    results: [],
+    speciesGrouup: null,
     totalRecords: 0
   },
 
   getters: {
+    catalogNumber(state) {
+      return state.catalogNumber
+    },
+    isAdvanceSearch(state) {
+      return state.isAdvanceSearch
+    },
+    results(state) {
+      return state.results
+    },
+    scientificName(state) {
+      return state.scientificName
+    },
+    searchText(state) {
+      return state.searchText
+    },
+    selectedResult(state) {
+      return state.selectedResult
+    },
     showDetail(state) {
       return state.showDetail
     },
     showResults(state) {
       return state.showResults
     },
-    results(state) {
-      return state.results
-    },
-    selectedResult(state) {
-      return state.selectedResult
+    speciesGrouup(state) {
+      return state.speciesGrouup
     },
     totalRecords(state) {
       return state.totalRecords
@@ -28,10 +48,15 @@ export default createStore({
   },
 
   mutations: {
+    setCatalogNumber: (state, payload) => (state.catalogNumber = payload),
+    setIsAdvanceSearch: (state, payload) => (state.isAdvanceSearch = payload),
+    setResults: (state, payload) => (state.results = payload),
+    setScientificName: (state, payload) => (state.scientificName = payload),
+    setSearchText: (state, payload) => (state.searchText = payload),
+    setSelectedResult: (state, payload) => (state.selectedResult = payload),
     setShowDetail: (state, payload) => (state.showDetail = payload),
     setShowResults: (state, payload) => (state.showResults = payload),
-    setResults: (state, payload) => (state.results = payload),
-    setSelectedResult: (state, payload) => (state.selectedResult = payload),
+    setSpeciesGroup: (state, payload) => (state.speciesGrouup = payload),
     setTotalRecords: (state, payload) => (state.totalRecords = payload)
   }
 
