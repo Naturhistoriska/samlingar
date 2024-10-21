fc7339791358870d06ed706ad5b9f668d8ab46d3
Ida Li <ida.li@nrm.se>
2024-10-21 10:42:35 +0200

    Add export service

--- samlingar_frontend/src/Service.js
+++ samlingar_frontend/src/Service.js
@@ -117,6 +117,124 @@ export default class Service {
     return response.data
   }
 
+  async export (
+    scientificName,
+    species_group,
+    dataset,
+    catalogNumber,
+    startDate,
+    endDate,
+    isType,
+    selectedCollection,
+    selectedTypeStatus,
+    total
+  ) {
+    let url = `${baseUrl}/search?q=data_hub_uid:${institutionId}`
+
+    if (scientificName) {
+      url = url + ` AND taxa:${scientificName}`
+    }
+
+    if (species_group) {
+      url = url + ` AND species_group:${species_group}`
+    }
+
+    if (dataset) {
+      url = url + ` AND collectionName:"${dataset}"`
+    }
+
+    if (catalogNumber) {
+      url = url + ` AND catalogue_number:"${catalogNumber}"`
+    }
+
+    if (startDate && endDate) {
+      url = url + ` AND occurrence_date:%5B${startDate}T00:00:00Z TO ${endDate}T00:00:00Z%5D`
+    } else if (startDate && !endDate) {
+      url = url + ` AND occurrence_date:%5B${startDate}T00:00:00Z TO *%5D`
+    } else if (!startDate && endDate) {
+      url = url + ` AND occurrence_date:%5B* TO ${endDate}T00:00:00Z%5D`
+    }
+
+    if (isType) {
+      url = url + '&fq=typeStatus:*'
+    }
+
+    if (selectedCollection) {
+      url += `&fq=collection_name:"${selectedCollection}"`
+    }
+
+    if (selectedTypeStatus) {
+      url += `&fq=typeStatus:"${selectedTypeStatus}"`
+    }
+
+
+    url += `&start=0&pageSize=${total}&fl=id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20classs%2C%20order%2C%20family%2C%20genus%2C%20species`
+    const response = await axios.get(url)
+
+    return response.data
+  }
+
+  async advanceCoordinatesSearch(
+    scientificName,
+    species_group,
+    dataset,
+    catalogNumber,
+    startDate,
+    endDate,
+    isType,
+    selectedCollection,
+    selectedTypeStatus,
+    coordinates,
+    total
+  ) {
+    let url = `${baseUrl}/search?q=data_hub_uid:${institutionId}`
+
+    if (scientificName) {
+      url = url + ` AND taxa:${scientificName}`
+    }
+
+    if (species_group) {
+      url = url + ` AND species_group:${species_group}`
+    }
+
+    if (dataset) {
+      url = url + ` AND collectionName:"${dataset}"`
+    }
+
+    if (catalogNumber) {
+      url = url + ` AND catalogue_number:"${catalogNumber}"`
+    }
+
+    if (startDate && endDate) {
+      url = url + ` AND occurrence_date:%5B${startDate}T00:00:00Z TO ${endDate}T00:00:00Z%5D`
+    } else if (startDate && !endDate) {
+      url = url + ` AND occurrence_date:%5B${startDate}T00:00:00Z TO *%5D`
+    } else if (!startDate && endDate) {
+      url = url + ` AND occurrence_date:%5B* TO ${endDate}T00:00:00Z%5D`
+    }
+
+    if (isType) {
+      url = url + '&fq=typeStatus:*'
+    }
+
+    if (selectedCollection) {
+      url += `&fq=collection_name:"${selectedCollection}"`
+    }
+
+    if (selectedTypeStatus) {
+      url += `&fq=typeStatus:"${selectedTypeStatus}"`
+    }
+
+    if (coordinates) {
+      url += `&fq=point-0.01:${coordinates}`
+    }
+
+    url += `&start=0&pageSize=${total}`
+    const response = await axios.get(url)
+
+    return response.data
+  }
+
   async advancesearchMapData(
     scientificName,
     species_group,
@@ -243,9 +361,7 @@ export default class Service {
     if (isType) {
       url += '&fq=typeStatus:*'
     }
-    url =
-      url +
-      `&start=${start}&pageSize=${rows}&fl=id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20classs%2C%20order%2C%20family%2C%20genus%2C%20species&facets=collectionName,point-0.01,typeStatus&flimit=3500`
+    url += `&start=${start}&pageSize=${rows}&fl=id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20classs%2C%20order%2C%20family%2C%20genus%2C%20species&facets=collectionName,point-0.01,typeStatus&flimit=3500`
     // url = url + `&start=${start}&pageSize=${rows}&facets=collectionName,year,lat_long,typeStatus`
     const response = await axios.get(url)
 
@@ -283,11 +399,11 @@ export default class Service {
     }
 
     if (typeStatus) {
-      url += `&fq=typeStatus:"${typeStatus}"`
+      url += `&fq=typeStatus:${typeStatus}`
     }
 
     url +=
-      '&fl=id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20classs%2C%20order%2C%20family%2C%20genus%2C%20species&&facets=collectionName,point-0.01,typeStatus&flimit=3500'
+      '&fl=id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20classs%2C%20order%2C%20family%2C%20genus%2C%20species&facets=collectionName,point-0.01,typeStatus&flimit=3500'
     // if (year) {
     //   if (year === 'Not supplied') {
     //     url += '&fq=-year:*'
