1a3ac78865dd6017964ef9387281f47ee4ea768b
Elin Ohlsson <elin.ohlsson@nrm.se>
2024-11-19 16:53:00 +0100

    Changes to About page

--- samlingar_frontend/src/views/About.vue
+++ samlingar_frontend/src/views/About.vue
@@ -1,3 +1,16 @@
 <template>
-  <div id="container" class="container container--fluid">About</div>
+  <div id="container" class="container container--fluid"> 
+  <h2> {{ $t('about.header') }}</h2>
+  <br>
+  <h3> {{ $t('about.header2') }}</h3>
+  
+<br>
+{{ $t('collectionName.Algae Collection') }}<br>
+{{ $t('collectionName.Entomological Collections (NRM)') }}<br>
+{{ $t('collectionName.Fish Collection NRM') }}<br>
+{{ $t('collectionName.Fishbase Database') }}<br>
+{{ $t('collectionName.Fungi Collection') }}<br>
+
+  
+  </div>
 </template>
