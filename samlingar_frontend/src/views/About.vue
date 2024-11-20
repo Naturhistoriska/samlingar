3d20cefa5dd39c108d22f1e9aa891b47931a4a2e
Ida Li <ida.li@nrm.se>
2024-11-20 15:47:03 +0100

    Update About.vue

--- samlingar_frontend/src/views/About.vue
+++ samlingar_frontend/src/views/About.vue
@@ -1,35 +1,32 @@
 <template>
-  <div id="container" class="container container--fluid"> 
-  <h2> {{ $t('about.header') }}</h2>
-  <br>
-  <h3> {{ $t('about.header2') }}</h3>
-  
-<br>
-{{ $t('collectionName.Algae Collection') }}<br>
-{{ $t('collectionName.Entomological Collections (NRM)') }}<br>
-{{ $t('collectionName.Fish Collection NRM') }}<br>
-{{ $t('collectionName.Fishbase Database') }}<br>
-{{ $t('collectionName.Fungi Collection') }}<br>
-{{ $t('collectionName.Herpetology Collection NRM') }}<br>
-{{ $t('collectionName.Invertebrates Collection NRM') }}<br>
-{{ $t('collectionName.Invertebrates Type Specimen Collection NRM') }}<br>
-{{ $t('collectionName.Lepidoptera Collection NRM') }}<br>
-{{ $t('collectionName.Lepidoptera Observations') }}<br>
-{{ $t('collectionName.Moss Collection') }}<br>
-{{ $t('collectionName.Phanerogamic Botanical Collections') }}<br>
-{{ $t('collectionName.Palaeozoological Collection') }}<br>
-{{ $t('collectionName.paleobotany') }}<br>
-{{ $t('collectionName.Porpoise Observation Database') }}<br>
-{{ $t('collectionName.Swedish Bird Ringing Centre') }}<br>
-{{ $t('collectionName.Swedish Malaise Trap Project - Collection Inventory') }}<br>
-{{ $t('collectionName.bird') }}<br>
-{{ $t('collectionName.mammals') }}<br>
-{{ $t('collectionName.amphibians') }}<br>
-{{ $t('collectionName.isotope') }}<br>
-{{ $t('collectionName.nodules') }}<br>
-{{ $t('collectionName.mineralogy') }}<br>
+  <div id="container" class="container container--fluid">
+    <h2>{{ $t('about.header') }}</h2>
+    <br />
+    <h3>{{ $t('about.header2') }}</h3>
 
-
-  
+    <br />
+    {{ $t('collectionName.Algae Collection') }}<br />
+    {{ $t('collectionName.Entomological Collections (NRM)') }}<br />
+    {{ $t('collectionName.Fish Collection NRM') }}<br />
+    {{ $t('collectionName.Fishbase Database') }}<br />
+    {{ $t('collectionName.Fungi Collection') }}<br />
+    {{ $t('collectionName.Herpetology Collection NRM') }}<br />
+    {{ $t('collectionName.Invertebrates Collection NRM') }}<br />
+    {{ $t('collectionName.Invertebrates Type Specimen Collection NRM') }}<br />
+    {{ $t('collectionName.Lepidoptera Collection NRM') }}<br />
+    {{ $t('collectionName.Lepidoptera Observations') }}<br />
+    {{ $t('collectionName.Moss Collection') }}<br />
+    {{ $t('collectionName.Phanerogamic Botanical Collections') }}<br />
+    {{ $t('collectionName.Palaeozoological Collection') }}<br />
+    {{ $t('collectionName.paleobotany') }}<br />
+    {{ $t('collectionName.Porpoise Observation Database') }}<br />
+    {{ $t('collectionName.Swedish Bird Ringing Centre') }}<br />
+    {{ $t('collectionName.Swedish Malaise Trap Project - Collection Inventory') }}<br />
+    {{ $t('collectionName.bird') }}<br />
+    {{ $t('collectionName.mammals') }}<br />
+    {{ $t('collectionName.amphibians') }}<br />
+    {{ $t('collectionName.isotope') }}<br />
+    {{ $t('collectionName.nodules') }}<br />
+    {{ $t('collectionName.mineralogy') }}<br />
   </div>
 </template>
