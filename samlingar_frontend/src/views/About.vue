9d11db34068206003c54ff7618b606bcdbc5cdc8
Elin Ohlsson <elin.ohlsson@nrm.se>
2024-11-20 10:31:39 +0100

    Changed the information in About.vue

--- samlingar_frontend/src/views/About.vue
+++ samlingar_frontend/src/views/About.vue
@@ -10,6 +10,25 @@
 {{ $t('collectionName.Fish Collection NRM') }}<br>
 {{ $t('collectionName.Fishbase Database') }}<br>
 {{ $t('collectionName.Fungi Collection') }}<br>
+{{ $t('collectionName.Herpetology Collection NRM') }}<br>
+{{ $t('collectionName.Invertebrates Collection NRM') }}<br>
+{{ $t('collectionName.Invertebrates Type Specimen Collection NRM') }}<br>
+{{ $t('collectionName.Lepidoptera Collection NRM') }}<br>
+{{ $t('collectionName.Lepidoptera Observations') }}<br>
+{{ $t('collectionName.Moss Collection') }}<br>
+{{ $t('collectionName.Phanerogamic Botanical Collections') }}<br>
+{{ $t('collectionName.Palaeozoological Collection') }}<br>
+{{ $t('collectionName.paleobotany') }}<br>
+{{ $t('collectionName.Porpoise Observation Database') }}<br>
+{{ $t('collectionName.Swedish Bird Ringing Centre') }}<br>
+{{ $t('collectionName.Swedish Malaise Trap Project - Collection Inventory') }}<br>
+{{ $t('collectionName.bird') }}<br>
+{{ $t('collectionName.mammals') }}<br>
+{{ $t('collectionName.amphibians') }}<br>
+{{ $t('collectionName.isotope') }}<br>
+{{ $t('collectionName.nodules') }}<br>
+{{ $t('collectionName.mineralogy') }}<br>
+
 
   
   </div>
