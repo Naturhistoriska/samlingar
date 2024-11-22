5a1c0269150e0710641d8bd1d3e1e7dc0d00c5ea
Ida Li <ida.li@nrm.se>
2024-11-22 10:11:30 +0100

    Change statistic path to initialData

--- samlingar_frontend/src/Service.js
+++ samlingar_frontend/src/Service.js
@@ -5,16 +5,16 @@ const speciesSearchUrl = import.meta.env.VITE_SBDI_SPECIES_SEARCH
 // const institutionId = import.meta.env.VITE_SUPPORTED_INSTITUTIONS
 const resultsPerPage = 10
 
-// const samlingApi = import.meta.env.VITE_SAMLINGAR_API_STAGE
-const samlingApi = import.meta.env.VITE_SAMLINGAR_API_LOCAL
+const samlingApi = import.meta.env.VITE_SAMLINGAR_API_STAGE
+// const samlingApi = import.meta.env.VITE_SAMLINGAR_API_LOCAL
 
 const fiedList =
   'id%2CcollectionName%2CcatalogNumber%2CscientificName%2C%20kingdom%2C%20phylum%2C%20class%2C%20order%2C%20family%2C%20genus%2C%20species'
 const facetList = 'collectionName,point-0.01,typeStatus,class,family,genus&flimit=40000'
 
 export default class Service {
-  async apiStatisticSearch() {
-    const url = `${samlingApi}/statistic`
+  async apiInitdata() {
+    const url = `${samlingApi}/initialData`
     const response = await axios.get(url)
     return response.data
   }
@@ -208,6 +208,14 @@ export default class Service {
     const response = await axios.get(url)
     return response.data
   }
+
+  async apiMonthChartData() {
+    let url = `${samlingApi}/statistic`
+    const response = await axios.get(url)
+    return response.data
+  }
+
+
 
   //
   //
