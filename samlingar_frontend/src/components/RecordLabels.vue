33a8dfb3c511856fdbdbf3cdcdfebe6652a1684d
Ida Li <ida.li@nrm.se>
2025-06-11 18:19:09 +0200

    Fix recordLabel

--- samlingar_frontend/src/components/RecordLabels.vue
+++ samlingar_frontend/src/components/RecordLabels.vue
@@ -1,60 +1,167 @@
 <template>
   <div class="card">
-    <!-- <DataTable
+    <DataView
       :value="records"
-      stripedRows
-      resizableColumns
-      columnResizeMode="fit"
-      scrollable
-      scrollHeight="400px"
-      tableStyle="min-width: 50rem"
-      :virtualScrollerOptions="{
-        lazy: true,
-        onLazyLoad: loadRecoudsLazy,
-        itemSize: 44,
-        delay: 200,
-        showLoader: true,
-        loading: lazyLoading,
-        numToleratedItems: 10
-      }"
+      :layout="layout"
+      paginator
+      :rows="30"
+      :paginator="true"
+      :lazy="true"
+      @page="onPage($event)"
+      :totalRecords="totalCount"
     >
-      <template #header>
-        <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-          <span class="text-xl text-900 font-bold">
-            {{ $t('results.searchResults') }} [{{ $t('results.num_results', totalCount) }}]
-          </span>
+      <template #grid="slotProps">
+        <div class="grid grid-cols-12">
+          <div
+            v-for="(item, index) in slotProps.items"
+            :key="index"
+            class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-6 p-2"
+          >
+            <div
+              class="flex flex-row justify-between items-start gap-3"
+              style="max-width: 350px; min-width: 350px"
+            >
+              <card style="height: 150px; width: 350px" v-if="item != undefined">
+                <template #title>
+                  <div class="grid">
+                    <div class="col-10" no-gutters>
+                      <small>{{ item.scientificName }}</small>
+                    </div>
+                    <div class="col-2" no-gutters>
+                      <Button variant="link" @click="view(item)">
+                        <small>{{ $t('records.view') }}</small>
+                      </Button>
+                    </div>
+                  </div>
+                </template>
+                <template #subtitle>
+                  <small>{{ item.collectionName }}</small>
+                </template>
+                <template #content>
+                  <div style="font-size: 0.7em">
+                    {{ item.catalogNumber }}
+                  </div>
+                  <div style="font-size: 0.7em">
+                    {{ item.locality }} {{ item.country }} {{ item.continent }}
+                  </div>
+                  <div v-if="item.decimalLatitude != null" style="font-size: 0.7em">
+                    Lat: {{ item.decimalLatitude }} Lon: {{ item.decimalLongitude }}
+                  </div>
+                </template>
+              </card>
+            </div>
+          </div>
         </div>
       </template>
-
-      <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header">
-        <template #loading>
-          <div
-            class="flex items-center"
-            :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }"
-          >
-            <Skeleton width="60%" height="1rem" />
-          </div>
-        </template>
-      </Column>
-    </DataTable> -->
+    </DataView>
   </div>
 </template>
+
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import Service from '../Service'
 
 const store = useStore()
+const router = useRouter()
 
-let records = ref(Array.from({ length: 100000 }))
+const service = new Service()
 
-onMounted(() => {
-  console.log('labels onMounted')
+let records = ref(Array.from({ length: 50 }))
+
+const layout = ref('grid')
+
+const searchText = computed(() => {
+  let text = store.getters['searchText']
+  return text ? text : '*'
 })
+
+const scientificName = computed(() => {
+  return store.getters['scientificName']
+})
+
+const isFuzzySearch = computed(() => {
+  return store.getters['isFuzzySearch']
+})
+
+const hasImages = computed(() => {
+  return store.getters['filterImage']
+})
+
+const hasCoordinates = computed(() => {
+  return store.getters['filterCoordinates']
+})
+
+const isInSweden = computed(() => {
+  return store.getters['filterInSweden']
+})
+
+const isType = computed(() => {
+  return store.getters['filterType']
+})
+
+onMounted(async () => {
+  console.log('labels onMounted')
+
+  await new Promise((res) => setTimeout(res, 500))
+  fetchData(0, 30)
+})
+
+function fetchData(start, end) {
+  service
+    .apiSearch(
+      searchText.value,
+      scientificName.value,
+      isFuzzySearch.value,
+      hasImages.value,
+      hasCoordinates.value,
+      isType.value,
+      isInSweden.value,
+      start,
+      end
+    )
+    .then((response) => {
+      const total = response.response.numFound
+      const results = response.response.docs
+
+      store.commit('setResults', results)
+      // store.commit('setTotalRecords', total)
+
+      records.value = results
+    })
+    .catch()
+    .finally(() => {})
+}
+
+const onPage = async (event) => {
+  console.log('event')
+  const { first, rows } = event
+  await new Promise((res) => setTimeout(res, 500))
+
+  fetchData(first, rows)
+  // service
+  //   .apiSearch(searchText.value, null, true, false, false, false, false, first, rows)
+  //   .then((response) => {
+  //     const total = response.response.numFound
+  //     const results = response.response.docs
+
+  //     store.commit('setResults', results)
+  //     store.commit('setTotalRecords', total)
+
+  //     records.value = results
+  //   })
+  //   .catch()
+  //   .finally(() => {})
+}
 
 const totalCount = computed(() => {
   return store.getters['totalRecords']
 })
 
-const loadRecoudsLazy = (event) => {}
+function view(data) {
+  store.commit('setSelectedRecord', data)
+  router.push(`/record/${data.id}`)
+}
 </script>
 <style scoped></style>
