029258243911ea1afad6938fc7093e1030c99484
Ida Li <ida.li@nrm.se>
2025-07-02 14:15:02 +0200

    Update Dataset.vue

--- samlingar_frontend/src/components/Dataset.vue
+++ samlingar_frontend/src/components/Dataset.vue
@@ -1,92 +1,208 @@
 <template>
-  <div class="card flex justify-center selectGroup">
-    <label class="searchLabel">
-      {{ $t('search.datasetName') }}
-    </label>
-    <Select
-      id="groupDataset"
-      v-model="selectedGroupDataset"
-      :options="groupDataset"
-      optionLabel="label"
-      optionGroupLabel="label"
-      optionGroupChildren="items"
-      :placeholder="$t('search.selectDatasetName')"
-      checkmark
-      :highlightOnSelect="true"
-      class="w-full md:w-56"
-      showClear
-      @change="onChange"
-      style="margin-left: 7px"
-    />
+  <div style="font-size: 12px">
+    <p style="font-weight: bold; font-size: 1em">{{ $t('results.dataset') }}</p>
+    <div class="grid">
+      <div class="col-3 reducePadding">{{ $t('results.dataResource') }}</div>
+      <div class="col-9 reducePadding">
+        {{ dataResource }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.institiution') }}</div>
+      <div class="col-9 reducePadding">
+        {{ institution }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.collectionName') }}</div>
+      <div class="col-9 reducePadding">
+        {{ collection }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.catalogNumber') }}</div>
+      <div class="col-9 reducePadding">
+        {{ catNumber }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.recordedType') }}</div>
+      <div class="col-9 reducePadding">
+        {{ recordType }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.preparation') }}</div>
+      <div class="col-9 reducePadding">
+        {{ preparationList }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.identifiedBy') }}</div>
+      <div class="col-9 reducePadding">
+        {{ identifier }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.recordedBy') }}</div>
+      <div class="col-9 reducePadding">
+        {{ collectors }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.sex') }}</div>
+      <div class="col-9 reducePadding">
+        {{ specimenSex }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.lifeStage') }}</div>
+      <div class="col-9 reducePadding">
+        {{ stage }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.reproductiveCondition') }}</div>
+      <div class="col-9 reducePadding">
+        {{ reproductCondition }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.individualCount') }}</div>
+      <div class="col-9 reducePadding">
+        {{ count }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.license') }}</div>
+      <div class="col-9 reducePadding">
+        {{ specimenLicense }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.modified') }}</div>
+      <div class="col-9 reducePadding">
+        {{ modifiedDate }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.dateIdentified') }}</div>
+      <div class="col-9 reducePadding">
+        {{ identifiedDate }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.previousIdentifications') }}</div>
+      <div class="col-9 reducePadding">
+        {{ additionalDetermination }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.occurrenceAttributeRemarks') }}</div>
+      <div class="col-9 reducePadding">
+        {{ occurrenceAttRemarks }}
+      </div>
+
+      <div class="col-3 reducePadding">{{ $t('results.occurrenceRemarks') }}</div>
+      <div class="col-9 reducePadding">
+        {{ remarks }}
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
 const store = useStore()
-let selectedGroupDataset = ref()
 
-const groupDataset = ref([
-  {
-    label: 'Zoological Collection',
-    items: [
-      { label: 'Amphibians and reptiles', value: 'herps' },
-      { label: 'Bird', value: 'va' },
-      { label: 'Fish', value: 'fish' },
-      { label: 'Invertebrate main collection', value: 'ev' },
-      { label: 'Invertebrate type collection', value: 'et' },
-      { label: 'Mammals', value: 'ma' },
-      { label: 'NRM Entomology Collection Objects', value: '163840' },
-      { label: 'Swedish Malaise Trap Project (SMTP) Collection Obj', value: '262144' },
-      { label: 'Swedish Malaise Trap Project (SMTP) Species Lists', value: '655361' }
-    ]
-  },
-  {
-    label: 'Botanical and mycological collections',
-    items: [
-      { label: 'Algae', value: 'algae' },
-      { label: 'Fungi/Lichens', value: 'fungi' },
-      { label: 'Mosses', value: 'mosses' },
-      { label: 'Vascular Plants', value: 'vp' }
-    ]
-  },
-  {
-    label: 'Palaeontological collection',
-    items: [
-      { label: 'Paleozoology', value: 'pz' },
-      { label: 'Paleobotany', value: 'pb' }
-    ]
-  },
-  {
-    label: 'Geological collection',
-    items: [
-      { label: 'NRM Isotope Geology', value: '753664' },
-      { label: 'NRM Mineralogy', value: '557057' },
-      { label: 'NRM Nodules', value: '786432' }
-    ]
-  }
-])
+const additionalDetermination = ref()
 
-function onChange() {
-  console.log('handleChange')
-  if (selectedGroupDataset.value) {
-    console.log('selectedGroupDataset', selectedGroupDataset.value.value)
-    const collectionCode = `%2BcollectionId:${selectedGroupDataset.value.value}`
-    store.commit('setSelectedDataset', collectionCode)
-  } else {
-    store.commit('setSelectedDataset', null)
-  }
-}
+const catNumber = ref()
+const code = ref()
+const collection = ref()
+const collectors = ref()
+const catalogedDate = ref()
+
+const dataResource = ref()
+
+const recordType = ref()
+const preparationList = ref()
+const identifier = ref()
+
+const specimenSex = ref()
+const stage = ref()
+const count = ref()
+const specimenLicense = ref()
+const modifiedDate = ref()
+const identifiedDate = ref()
+const institution = ref()
+
+const occurrenceAttRemarks = ref()
+const reproductCondition = ref()
+const remarks = ref()
+
+onMounted(async () => {
+  console.log('onMounted dataset')
+  const record = store.getters['selectedRecord']
+
+  const {
+    accessionNumber,
+    accessionRemarks,
+    basisOfRecord,
+    catalogNumber,
+    collectionCode,
+    collectionName,
+
+    dataResourceName,
+
+    dynamicProperties_createdDate,
+    dateIdentified,
+    identifiedBy,
+    individualCount,
+    institutionCode,
+    institutionName,
+
+    license,
+    lifeStage,
+    modified,
+    occurrenceAttributeRemarks,
+    occurrenceRemarks,
+    preparations,
+    prepCount,
+    previousIdentifications,
+    recordedBy,
+    reproductiveCondition,
+    sex
+  } = record
+
+  additionalDetermination.value = previousIdentifications
+
+  catNumber.value = catalogNumber
+  catalogedDate = dynamicProperties_createdDate
+  collection.value = collectionName
+  code.value = collectionCode
+
+  // dataResource.value = t('common.institution') + ' -' + collectionName
+  dataResource.value = dataResourceName
+  institution.value = institutionName + ' [ ' + institutionCode + ' ] '
+
+  recordType.value = basisOfRecord
+  preparationList.value =
+    collectionCode == 'NHRS'
+      ? prepCount
+        ? prepCount.toString()
+        : ''
+      : preparations
+        ? preparations.toString()
+        : ''
+
+  identifier.value = identifiedBy
+  collectors.value = recordedBy ? recordedBy.toString() : ''
+  specimenSex.value = sex
+  stage.value = lifeStage
+  count.value = individualCount
+  specimenLicense.value = license
+  modifiedDate.value = modified
+  identifiedDate.value = dateIdentified
+
+  // institution.value = institutionID + ' [ ' + institutionCode + ' ] '
+  // dataResource.value = institutionID + ' -' + collectionName
+  occurrenceAttRemarks.value = occurrenceAttributeRemarks
+  reproductCondition.value = reproductiveCondition
+  remarks.value = occurrenceRemarks
+})
 </script>
 <style scoped>
-.selectGroup {
-  max-width: 770px;
-  padding-bottom: 1rem;
-}
-.searchLabel {
-  padding-right: 2rem;
-  padding-top: 0.3rem;
-  text-align: right;
-  min-width: 150px;
+.reducePadding {
+  padding-top: 0px;
+  padding-bottom: 1px;
 }
 </style>
