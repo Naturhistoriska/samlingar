32a859de06864acc05dde6cca81cd15022b3ebfa
Ida Li <ida.li@nrm.se>
2025-09-24 17:18:30 +0200

    Change logo

--- samlingar_frontend/src/components/Header.vue
+++ samlingar_frontend/src/components/Header.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="grid nested-grid divbg nesteddiv">
     <div class="col-1 pt-5 pb-5 logodiv">
-      <!-- <img src="/nrm-logo-liggande-svensk-negativ.png" /> -->
-      <img src="/nrm-logo.png" />
+      <img src="/nrm-logo-liggande-svensk-negativ_copy.png" width="280" />
+      <!-- <img src="/nrm-logo.svg" width="300" /> -->
     </div>
-    <div class="col-11">
-      <div class="grid">
-        <div class="col-12 pt-3" style="width: 100%; min-height: 20px"><br /></div>
+    <div class="col-11 pt-6">
+      <div class="col-5 text-right" style="max-width: 800px; float: right">
+        <Search />
       </div>
-      <div class="grid">
+      <!-- <div class="grid">
+        <div class="col-12 pt-5" style="width: 100%; min-height: 20px"><br /></div>
+      </div> -->
+      <!-- <div class="grid">
         <div class="flex flex-column col-7"></div>
-        <div class="col-5 text-right" style="max-width: 600px; float: right">
-          <Search />
-        </div>
-      </div>
-      <div class="grid">
+
+      </div> -->
+      <!-- <div class="grid">
         <div class="col-12 text-left">
           <span class="title-text">{{ $t('title') }}</span>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -42,8 +43,11 @@ export default {
 }
 .logodiv {
   max-width: 100px;
+  padding-left: 5rem;
+  padding-top: 2rem;
 }
 .divbg {
+  /* background-color: #144836; */
   background-color: #144836;
   max-height: 8rem;
 }
