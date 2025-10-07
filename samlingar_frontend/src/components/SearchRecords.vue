ccdfc3d57500c5834e1895532357355498e52811
Ida Li <ida.li@nrm.se>
2025-10-07 12:36:28 +0200

    Do search when selected collections changes

--- samlingar_frontend/src/components/SearchRecords.vue
+++ samlingar_frontend/src/components/SearchRecords.vue
@@ -58,7 +58,7 @@
           toggleable
           :collapsed="collectionGroupPanelNotVisible"
         >
-          <Collections />
+          <Collections @collectionsSearch="search" />
         </Panel>
 
         <Panel
