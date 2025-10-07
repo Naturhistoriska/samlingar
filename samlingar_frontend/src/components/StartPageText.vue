a5701e30ebce485aa19779db8a7eeb7b3c915c67
Ida Li <ida.li@nrm.se>
2025-10-07 12:35:42 +0200

    Update locales

--- samlingar_frontend/src/components/StartPageText.vue
+++ samlingar_frontend/src/components/StartPageText.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="grid">
     <div class="col-12" no-gutters>
-      <p class="m-0 textStyle">{{ $t('startPage.text') }}</p>
+      <p class="m-0 textStyle">{{ $t('startPage.text1') }}</p>
+      <br />
+      <p class="m-0 textStyle">{{ $t('startPage.text2') }}</p>
+      <br />
+      <p class="m-0 textStyle">{{ $t('startPage.text3') }}</p>
+      <br />
+      <p class="m-0 textStyle">{{ $t('startPage.text4') }}</p>
     </div>
   </div>
 </template>
