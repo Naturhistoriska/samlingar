5199ebcd26f81d38d5375eecff1443ec1fffc324
Ida Li <ida.li@nrm.se>
2025-11-14 18:26:15 +0100

    Update MapSwitch.vue

--- samlingar_frontend/src/components/MapSwitch.vue
+++ samlingar_frontend/src/components/MapSwitch.vue
@@ -1,63 +1,15 @@
 <template>
-  <div v-if="isHeatMap">
-    <large-map v-bind:entry="entryType" v-bind:from="from" />
-  </div>
-  <div v-else>
-    <Map v-bind:entry="entryType" v-bind:from="from" />
-  </div>
+  <component :is="Component" />
 </template>
+
 <script setup>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { useStore } from 'vuex'
-import { entryType, previousRoute, currentRoute } from '@/router'
 
 import Map from '../components/Map2.vue'
 import LargeMap from '../components/NewMap.vue'
 
 const store = useStore()
 
-// let isLargeMap = ref(true)
-let reloadMapData = ref(false)
-const from = ref()
-
-const isHeatMap = computed(() => {
-  const total = store.getters['totalGeoData']
-  console.log('is heat map', total >= 50000)
-  return store.getters['totalGeoData'] >= 50000
-})
-
-// watch(
-//   () => store.getters['totalRecords'],
-//   () => {
-//     const total = store.getters['totalRecords']
-//     console.log('total', total)
-//     isLargeMap.value = total > 50000
-//   }
-// )
-
-// watch(
-//   () => store.getters['resetMapData'],
-//   () => {
-//     console.log('resetMapData..')
-//     reloadMapData.value = true
-//     // const total = store.getters['totalGeoData'] > 50000
-
-//     // if (total > 50000) {
-//     //   reloadMapData.value = true
-//     // }
-//     //
-//   }
-// )
-
-// onMounted(async () => {
-//   console.log('switch map onMounted')
-
-//   from.value = previousRoute.value?.fullPath
-//   // const to = currentRoute.value?.fullPath
-//   console.log('from', from)
-
-//   const total = store.getters['totalGeoData']
-//   console.log('total', total)
-//   // isLargeMap.value = total >= 50000
-// })
+const Component = computed(() => (store.getters['totalGeoData'] >= 50000 ? LargeMap : Map))
 </script>
