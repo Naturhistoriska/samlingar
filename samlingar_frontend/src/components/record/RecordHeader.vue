72b55a9588a08458d0ca9adecd5203c2ac294c7e
Ida Li <ida.li@nrm.se>
2025-11-16 12:33:14 +0100

    Code refactor

---
+++ samlingar_frontend/src/components/record/RecordHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid">
+    <p class="m-0 titleStyle col-12">{{ $t('records.recordTitle') }}</p>
+
+    <div class="classificationText col-12">{{ classification }}</div>
+
+    <div class="scientificNameText col-12">
+      <i>{{ name }}</i>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  classification: String,
+  name: String
+})
+</script>
+
+<style scoped>
+.titleStyle {
+  font-weight: bold;
+  font-size: 2em;
+  color: #777676;
+}
+.classificationText {
+  font-size: 1em;
+  color: #6fade6;
+}
+.scientificNameText {
+  font-size: 1.5em;
+  font-weight: bold;
+  color: #6fade6;
+}
+
+@media (max-width: 768px) {
+  .titleStyle {
+    font-size: 1.2rem;
+  }
+
+  .classificationText {
+    font-size: 0.7em;
+  }
+
+  .scientificNameText {
+    font-size: 0.8em;
+    font-weight: bold;
+    color: #6fade6;
+  }
+}
+</style>
---
+++ samlingar_frontend/src/components/record/RecordLeft.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-12 md:col-7">
+    <record-dataset :code="code" />
+    <record-event :code="code" />
+    <location :code="code" />
+
+    <geological-context v-if="isPalCollection" />
+
+    <record-identification :code="code" />
+    <taxonomy :code="code" />
+  </div>
+</template>
+
+<script setup>
+import Location from '../Location.vue'
+import GeologicalContext from '../GeologicalContext.vue'
+import RecordDataset from '../RecordDataset.vue'
+import RecordEvent from '../RecordEvent.vue'
+import RecordIdentification from '../RecordIdentification.vue'
+import Taxonomy from './Taxonomy.vue'
+
+defineProps({
+  code: String,
+  isPalCollection: Boolean
+})
+</script>
