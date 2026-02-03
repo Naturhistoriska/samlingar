c18d0636f7d827bf3d47e4bd1228fdbc26c86537
Ida Li <ida.li@nrm.se>
2026-02-03 14:34:36 +0100

    Update .eslintrc.cjs

--- samlingar_frontend/.eslintrc.cjs
+++ samlingar_frontend/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vuejs-accessibility/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
