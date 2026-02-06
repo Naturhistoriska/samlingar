c3785fa56a44a198650914864b93318a2b39781f
Ida Li <ida.li@nrm.se>
2026-02-06 23:25:59 +0100

    Update vite.config.js

--- samlingar_frontend/vite.config.js
+++ samlingar_frontend/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve, dirname } from 'node:path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
+import pkg from './package.json'
 
 // import Components from 'unplugin-vue-components/vite'
 // import { transformAssetUrls } from 'vite-plugin-vuetify'
@@ -23,12 +24,16 @@ export default defineConfig({
     vue({
       // template: { transformAssetUrls }
     }),
+
     // Components(),
     VueI18nPlugin({
       // runtimeOnly: false, // <--- add this if localization in production not working
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**') // provide a path to the folder where you'll store translation data (see below)
     })
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
