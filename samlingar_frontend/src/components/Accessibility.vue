700d5b08c4172b7ef2a6742cf24fc4d99dff801f
Ida Li <ida.li@nrm.se>
2026-02-06 23:18:24 +0100

    Create Accessibility.vue

---
+++ samlingar_frontend/src/components/Accessibility.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="page-container">
+    <h2 class="card-title">Accessibility of samlingar.nrm.se</h2>
+    <div class="card-text">
+      <p>
+        This website is run by Swedish Museum of Natural History. We want as many people as possible
+        to be able to use it, and this document describes how samlingar.nrm.se complies with the
+        accessibility regulations, any known accessibility issues, and how you can report problems
+        so that we can fix them.
+      </p>
+    </div>
+
+    <h2 class="card-title">How accessible is the website?</h2>
+    <div class="card-text">
+      <p>
+        We know some parts of this website aren’t fully accessible. See the section on
+        non-accessible content below for more information.
+      </p>
+    </div>
+
+    <h2 class="card-title">What to do if you can’t access parts of this website?</h2>
+    <div class="card-text">
+      <p>
+        If you need content from this website that is not accessible for you, but is not within the
+        scope of the accessibility regulations as described below, please contact
+        webbredaktionen@nrm.se
+      </p>
+
+      <p>Our normal response time is 3 days.</p>
+
+      <ul>
+        <li>email: bioinformatics@nrm.se</li>
+        <li>call: 46-8-51 95 40 00</li>
+      </ul>
+    </div>
+
+    <h2 class="card-title">Enforcement procedure</h2>
+    <div class="card-text">
+      <p>
+        The Agency for Digital Government, Digg, is responsible for enforcing the web accessibility
+        regulations. If you experience accessibility issues on our website, you can
+
+        <a href="https://www.digg.se/tdosanmalan" target="_blank"> submit a complaint to Digg</a>.
+      </p>
+      <p>
+        You can also
+        <a href="https://www.digg.se/tdosanmalan" target="_blank"> submit a complaint to Digg</a>
+        if you think that our assessment of what constitutes a disproportionate burden should be
+        reviewed, if you think that our accessibility statement is inadequate, or it you think that
+        your request for excluded content in an accessible format has not been handled correctly.
+      </p>
+    </div>
+
+    <h2 class="card-title">Technical information about this website’s accessibility</h2>
+    <div class="card-text">
+      <p>
+        This website is not compliant with the Swedish accessibility regulations. The non-accessible
+        sections are listed below.
+      </p>
+    </div>
+
+    <h2 class="card-title">Non-accessible content</h2>
+    <div class="card-text">
+      <p>The content described below is, in one way or another, not fully accessible.</p>
+    </div>
+
+    <h2 class="card-title">All information can be accessed and used regardless of screen size</h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that the website can be viewed and used with a screen
+        width of 320 css px and wider.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      The interface should be viewable and usable without the user needing to scroll in more than
+      one direction.
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that horizontal scroll is an option when the content does
+        not fit the screen.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      The website is fully usable and readable when zoomed in up to 200 percent.
+    </h2>
+
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that information is able to access with browser zoom
+        settings up to 200 percent.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      The interface can be controlled with the keyboard on both mobile and large screens
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that clickable objects can have focus by using the
+        keyboard and that triggering events from focused objects can be made by keystrokes.
+      </p>
+    </div>
+
+    <h2 class="card-title">There are shortcuts to make navigation easier with the keyboard</h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to implement hidden anchor links that helps visitors using keyboard to
+        navigate to the exclusive content on each page.
+      </p>
+    </div>
+
+    <h2 class="card-title">Link groups and information areas are grouped</h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to add landmarks to link groups and information areas to help people
+        easily navigate within the structure of a single page.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      The foreground and background colors of the text together provide sufficient contrast
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed on some areas to make sure that the contrast is not less than 4,5:1.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      Highlighting of clickable objects and adjacent colors should have a contrast of at least 3.0:1
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that all borders surrounding form fields holds a contrast
+        not less than 3:1 to adjacent colors.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      Understanding content is not dependent on the user's ability to perceive colors
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that all information can be interpreted independent of
+        ability to perceive colors.
+      </p>
+    </div>
+
+    <h2 class="card-title">The pages have unique and relevant page titles</h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that the titles is reflecting the content of the unique
+        page.
+      </p>
+    </div>
+
+    <h2 class="card-title">Text that visually functions as headings is also coded as headings</h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that the correct heading codes (H1, H2, H3 etc) are used
+        in the content.
+      </p>
+    </div>
+
+    <h2 class="card-title">The heading structure begins with a main heading</h2>
+    <div class="card-text">
+      <p>Our action is needed to make sure that the heading structure starts with H1.</p>
+    </div>
+
+    <h2 class="card-title">
+      The heading structure is clear and reflects the hierarchy of the content
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that the heading structure follows a hierarchy with no
+        gaps between heading levels (for example H1 > H3)
+      </p>
+    </div>
+
+    <h2 class="card-title">Lists are correctly coded and used correctly</h2>
+    <div class="card-text">
+      <p>Our action is needed to add list definitions in the coding where lists are presented.</p>
+    </div>
+
+    <h2 class="card-title">
+      Equivalent text descriptions exist for all meaningful graphic elements on the website
+    </h2>
+    <div class="card-text">
+      <p>
+        Our action is needed to make sure that the content is understandable without the option to
+        view graphics et c. Diagrams etc need a written equivalent.
+      </p>
+    </div>
+
+    <h2 class="card-title">Form objects are coded with the correct form code</h2>
+    <div class="card-text">
+      <p>Our action is needed to make sure that form objects are correctly coded.</p>
+    </div>
+
+    <h2 class="card-title">Prompts are tied to the respective form object</h2>
+    <div class="card-text">
+      <p>We need to add label elements to connect prompts with form objects.</p>
+    </div>
+
+    <h2 class="card-title">Button functions are clearly visible</h2>
+    <div class="card-text">
+      <p>
+        We need to add accessibility names to all our buttons to make sure that they can be
+        understood with different equipment.
+      </p>
+    </div>
+
+    <h2 class="card-title">Parts in the forms are grouped</h2>
+    <div class="card-text">
+      <p>We need to group checkboxes in forms so that the functionality is easier to interpret.</p>
+    </div>
+
+    <h2 class="card-title">Table headings are coded with the caption element.</h2>
+    <div class="card-text">
+      <p>We need to add a caption element to our tables.</p>
+    </div>
+
+    <h2 class="card-title">
+      Use wai-aria to convey information that cannot be conveyed with html and that increases user
+      benefit
+    </h2>
+    <div class="card-text">
+      <p>
+        We need to make sure that Wai-aria is only used when needed to complete what is not covered
+        by html coding.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      Content displayed when an object has focus is fully usable and accessible until the user moves
+      focus.
+    </h2>
+    <div class="card-text">
+      <p>
+        Action is needed to make tooltip info connected to statistic data readable even without
+        having focus.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      Aria-expanded is used for areas that can be folded out and folded in.
+    </h2>
+    <div class="card-text">
+      <p>
+        Especially in the mobile version we are not consequently using the attribute aria-expanded,
+        and we need to look this over.
+      </p>
+    </div>
+
+    <h2 class="card-title">
+      Languages that differ from the page's main language are specified in the code
+    </h2>
+    <div class="card-text">
+      <p>Lang-attribute and correct langue code is missing in the langue selector.</p>
+    </div>
+
+    <p>It is our ambition to fix all known accessibility problems by 2025-12-31</p>
+
+    <h2 class="card-title">How we tested this website</h2>
+    <div class="card-text">
+      <p>Funka has tested samlingar.nrm.se and the report is dated 2025-11-28 – 2025-12-09</p>
+    </div>
+
+    <h2 class="card-title">
+      <a href="https://example.com/report" target="_blank">Evaluation report</a>.
+    </h2>
+
+    <p>The website was published on 2025-10-17</p>
+    <p>The statement was last updated on 2026-02-06</p>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.page-container {
+  height: 90vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+/* Existing styles */
+.card-title {
+  font-size: 1.3rem;
+  margin-bottom: 1rem;
+}
+
+.card-text {
+  font-size: 1rem;
+  line-height: 1.5;
+  margin-bottom: 1rem;
+}
+
+/* Mobile */
+@media (max-width: 768px) {
+  .card-title {
+    font-size: 1rem;
+  }
+
+  .card-text {
+    font-size: 0.8rem;
+  }
+}
+</style>
